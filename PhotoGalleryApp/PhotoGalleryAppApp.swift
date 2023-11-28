62d9f7fc3197daca42bab4f83b42b482cffce2d5
Amish Gupta <amishgupta678@gmail.com>
2023-11-28 15:22:29 -0800

    Initial Commit

---
+++ PhotoGalleryApp/PhotoGalleryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoGalleryAppApp.swift
+//  PhotoGalleryApp
+//
+//  Created by Amish Gupta on 11/28/23.
+//
+
+import SwiftUI
+
+@main
+struct PhotoGalleryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
